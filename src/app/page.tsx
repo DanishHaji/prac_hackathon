1d4b934ed6f27429e3f9c20152fe04ca0e8a8913
DanishHaji <danish.haji111@gmail.com>
2025-01-13 19:25:32 +0500

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import DataFetch from "@/components/DataFetch";
-import Image from "next/image";
 
 export default function Home() {
   return (
--- src/components/DataFetch.tsx
+++ src/components/DataFetch.tsx
@@ -3,8 +3,16 @@ import React from 'react';
 import Image from 'next/image';
 import { urlFor } from '@/sanity/lib/image';
 
+interface Product {
+  _id: string;
+  name: string;
+  price: number;
+  discountPercentage: number;
+  imageUrl: string;
+}
+
 const DataFetch = async () => {
-  const query = await client.fetch(
+  const query = await client.fetch<Product[]>(
     `*[_type == "product"]{
       _id,
       name,
@@ -17,31 +25,29 @@ const DataFetch = async () => {
 
   return (
     <div className="grid grid-cols-1 bg-sky-50 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
-      {query.map((product: any) => {
-        return (
-          <div
-            key={product._id}
-            className="border rounded-lg bg-blue-100 shadow-md overflow-hidden transition-transform duration-300 hover:scale-105 hover:shadow-lg"
-          >
-            <Image
-              src={urlFor(product.imageUrl).url()}
-              alt={product.name}
-              width={200}
-              height={200}
-              className="w-full h-48 object-contain"
-            />
-            <div className="p-4">
-              <h1 className="text-lg font-bold text-gray-800 mb-2">{product.name}</h1>
-              <p className="text-gray-600">
-                <span className="font-semibold text-black">${product.price}</span>
-              </p>
-              <p className="text-sm text-red-500">
-                Save {product.discountPercentage}%!
-              </p>
-            </div>
+      {query.map((product : Product) => (
+        <div
+          key={product._id}
+          className="border rounded-lg bg-blue-100 shadow-md overflow-hidden transition-transform duration-300 hover:scale-105 hover:shadow-lg"
+        >
+          <Image
+            src={urlFor(product.imageUrl).url()}
+            alt={product.name}
+            width={200}
+            height={200}
+            className="w-full h-48 object-contain"
+          />
+          <div className="p-4">
+            <h1 className="text-lg font-bold text-gray-800 mb-2">{product.name}</h1>
+            <p className="text-gray-600">
+              <span className="font-semibold text-black">${product.price}</span>
+            </p>
+            <p className="text-sm text-red-500">
+              Save {product.discountPercentage}%!
+            </p>
           </div>
-        );
-      })}
+        </div>
+      ))}
     </div>
   );
 };
